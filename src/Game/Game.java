4d6cfedf3ea84e0eee6740456fa88a6b83ea9ccd
dugalde <dugalde@pernix-solutions.com>
2012-11-14 17:22:21 -0600

    First commit

---
+++ src/Game/Game.java
@@ -0,0 +1,67 @@
+package Game;
+
+/**
+ * User: David
+ * Date: 11/14/12
+ */
+public class Game {
+
+   // private int score = 0;
+    private int rolls [] = new int [21];
+    private int currentRoll = 0;
+    
+    
+    public void roll(int pins) {
+
+        rolls[currentRoll++] = pins;
+    }
+
+    private int twoBallsInFrame(int firstInFrame) {
+        return rolls[firstInFrame] + rolls[firstInFrame + 1];
+    }
+
+    private int nextBallForSpare(int firstInFrame) {
+        return rolls[firstInFrame + 2];
+    }
+
+    private int nextTwoBallForStrike(int firstInFrame) {
+        return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+    }
+
+    private boolean isStrike(int firstInFrame) {
+        return rolls[firstInFrame] == 10;
+    }
+
+    private boolean isSpare(int firstInFrame) {
+        return rolls[firstInFrame] + rolls[ firstInFrame + 1 ] == 10;
+    }
+
+    public int score() {
+        int score = 0;
+        int firstInFrame = 0;
+        for (int frame = 0; frame < 10 ; frame++) {
+
+            if (isStrike(firstInFrame)){
+               score += 10 + nextTwoBallForStrike(firstInFrame);
+                firstInFrame ++;
+
+            }
+            else if (isSpare(firstInFrame))
+            {
+                score +=  10 + nextBallForSpare(firstInFrame);
+                firstInFrame += 2 ;
+            }
+            else
+            {
+                score += twoBallsInFrame(firstInFrame);
+                firstInFrame += 2;
+            }
+
+        }
+            return score;
+    }
+
+
+
+
+}
